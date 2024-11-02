836bf2053ad438b313b095e69ef4ac60abb16cea
Manuela Grundbek <gru240833@spengergasse.at>
2024-11-02 15:52:10 +0100

    Initial sharing of project

---
+++ ClassRoom.java
@@ -0,0 +1,49 @@
+public class ClassRoom
+{
+    private String  hersteller;
+    private boolean laptop;
+    private int     preis;
+    private int     mhz;
+    
+    
+
+    public void setHersteller(String Hersteller)
+    {
+        hersteller = newHersteller;
+    }
+    
+    public void setLaptop(boolean newLaptop)
+    {
+        laptop = newLaptop;
+    }
+    
+    public void setPreis(int newPreis)
+    {
+        preis = newPreis;
+    }
+    
+    public void setMhz (int newMhz)
+    {
+        mhz = 2000;
+    }
+    
+    public String getHersteller()
+    {
+        return hersteller;
+    }
+    
+    public boolean getLaptop()
+    {
+        return laptop;
+    }
+    
+    public int getPreis()
+    {
+        return preis;
+    }    
+    
+    public int getMhz()
+    {
+        return 2000;
+    }
+}
